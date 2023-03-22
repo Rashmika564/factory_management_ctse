377ecd6cfea1c756a13581f3b3b8b7debc0635d1
chathura49620 <chathuraprabath49620@gmail.com>
2023-03-22 20:02:35 +0530

    main : appoinment model and db oparations create

---
+++ lib/data/remote_data_source/appoinment_helper.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/apponment_model.dart';
+
+
+class DoctorHelper {
+  static Stream<List<AppoinmentModel>> read() {
+    final doctorCollection = FirebaseFirestore.instance.collection("appoinments");
+    return doctorCollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => AppoinmentModel.fromSnapshot(e)).toList());
+  }
+
+  static Future create(AppoinmentModel appoinment) async {
+    final appoinmenntCollection = FirebaseFirestore.instance.collection("appoinments");
+
+    final did = appoinmenntCollection.doc().id;
+    final appoinementRef = appoinmenntCollection.doc(did);
+
+    final newAppoinment = AppoinmentModel(
+      id: did,
+      doctorName: appoinment.doctorName,
+      hospitalName: appoinment.hospitalName,
+      date: appoinment.date,
+      reson: appoinment.reson
+    ).toJson();
+
+    try {
+      await appoinementRef.set(newAppoinment);
+    } catch (e) {
+      print("some error occured when create appoinment");
+    }
+  }
+
+  static Future update(AppoinmentModel appoinment) async {
+    final appoinmenntCollection = FirebaseFirestore.instance.collection("appoinments");
+
+    final appoinmentRef = appoinmenntCollection.doc(appoinment.id);
+
+    final updateAppoinmentRec = AppoinmentModel(
+      id: appoinment.id,
+      doctorName: appoinment.doctorName,
+      hospitalName: appoinment.hospitalName,
+      date: appoinment.date,
+      reson: appoinment.reson
+    ).toJson();
+
+    try {
+      await appoinmentRef.update(updateAppoinmentRec);
+    } catch (e) {
+      print("some error occured");
+    }
+  }
+
+  static Future delete(AppoinmentModel appoinment) async {
+    final appoinmenntCollection = FirebaseFirestore.instance.collection("appoinments");
+    final appoinmentRef = appoinmenntCollection.doc(appoinment.id).delete();
+  }
+  
+}
