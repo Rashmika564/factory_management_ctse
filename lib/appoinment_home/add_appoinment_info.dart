d651f97a2a5fb831285f84170b932bd1b966199b
chathura49620 <chathuraprabath49620@gmail.com>
2023-03-23 00:37:51 +0530

    main : styling the add apoinment screen

--- lib/appoinment_home/add_appoinment_info.dart
+++ lib/appoinment_home/add_appoinment_info.dart
@@ -1,11 +1,11 @@
-
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:factory_management_ctse/appoinment_home/appoinment_list.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 import '../data/models/apponment_model.dart';
 import '../data/remote_data_source/appoinment_helper.dart';
 import '../services/auth.dart';
+import 'package:intl/intl.dart';
 
 final ButtonStyle flatButtonStyle = TextButton.styleFrom(
   // ignore: deprecated_member_use
@@ -26,10 +26,19 @@ class AddAppoinment extends StatefulWidget {
 }
 
 class _AddAppoinmentState extends State<AddAppoinment> {
-  final TextEditingController _doctorNamecontroller = TextEditingController(); 
+  TextEditingController _doctorNamecontroller = TextEditingController();
   final TextEditingController _hospitalNamecontroller = TextEditingController();
   final TextEditingController _datecontroller = TextEditingController();
   final TextEditingController _resoncontroller = TextEditingController();
+
+  List<String> items = [
+    'Item1',
+    'Item2',
+    'Item3',
+    'Item4',
+  ];
+  String? selectedValue;
+
   final AuthService service = AuthService();
   final _formKey = GlobalKey<FormState>();
 
@@ -69,6 +78,45 @@ class _AddAppoinmentState extends State<AddAppoinment> {
             key: _formKey,
             child: Column(
               children: [
+                Container(
+                  height: 300.0,
+                  decoration: const BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(250),
+                        bottomRight: Radius.circular(250),
+                      )),
+                ),
+                const SizedBox(height: 20.0),
+                const Text("Welcome",
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        letterSpacing: 1.5,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black)),
+                const SizedBox(height: 10.0),
+                const Text("Book Your Doctor Appoinment Hear",
+                    style: TextStyle(fontSize: 16.0, color: Colors.black)),
+                const SizedBox(height: 20.0),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      child: const Text(
+                        "View all",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.black),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const AppoinmentList()));
+                      }),
+                ),
+                // const Schedule(),
+                const SizedBox(height: 30.0),
+                // const Image(image: AssetImage('graphics/background.png')),
                 TextFormField(
                   controller: _hospitalNamecontroller,
                   decoration: const InputDecoration(
@@ -82,21 +130,88 @@ class _AddAppoinmentState extends State<AddAppoinment> {
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(), hintText: "Doctor Name"),
                 ),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                    hint: Text(
+                      'Select Item',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Theme.of(context).hintColor,
+                      ),
+                    ),
+                    items: items
+                        .map((item) => DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ))
+                        .toList(),
+                    value: selectedValue,
+                    onChanged: (value) {
+                      // setState(() {
+                        selectedValue = value as String?;
+                      // });
+                    },
+                  ),
+                ),
                 const SizedBox(
                   height: 10,
                 ),
                 TextFormField(
-                  controller: _datecontroller,
+                  controller:
+                      _datecontroller, //editing controller of this TextField
                   decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Date"),
+                      icon: Icon(Icons.calendar_today), //icon of text field
+                      labelText: "Enter Date" //label text of field
+                      ),
+                  readOnly:
+                      true, //set it true, so that user will not able to edit text
+                  onTap: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(
+                            2000), //DateTime.now() - not to allow to choose before today.
+                        lastDate: DateTime(2101));
+
+                    if (pickedDate != null) {
+                      if (kDebugMode) {
+                        print(pickedDate);
+                      } //pickedDate output format => 2021-03-10 00:00:00.000
+                      String formattedDate =
+                          DateFormat('yyyy-MM-dd').format(pickedDate);
+                      if (kDebugMode) {
+                        print(formattedDate);
+                      } //formatted date output using intl package =>  2021-03-16
+                      //you can implement different kind of Date Format here according to your requirement
+
+                      setState(() {
+                        _datecontroller.text =
+                            formattedDate; //set output date to TextField value.
+                      });
+                    } else {
+                      if (kDebugMode) {
+                        print("Date is not selected");
+                      }
+                    }
+                  },
                 ),
                 const SizedBox(
                   height: 10,
                 ),
                 TextFormField(
                   controller: _resoncontroller,
+                  minLines:
+                      6, // any number you need (It works as the rows for the textarea)
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
                   decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Reson for appoinment"),
+                      border: OutlineInputBorder(),
+                      hintText: "Reson for appoinment"),
                 ),
                 const SizedBox(
                   height: 10,
@@ -114,30 +229,22 @@ class _AddAppoinmentState extends State<AddAppoinment> {
                         reson: _resoncontroller.text));
 
                     Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                  const AppoinmentList()));
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AppoinmentList()));
                   },
                   child: Container(
                     width: 100,
-                    height: 30,
+                    height: 40,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: Colors.green),
+                        color: Colors.yellow),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: const [
-                        Icon(
-                          Icons.add,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
                         Text(
-                          "create",
-                          style: TextStyle(color: Colors.white),
+                          "Book",
+                          style: TextStyle(color: Colors.black),
                         ),
                       ],
                     ),
@@ -146,7 +253,6 @@ class _AddAppoinmentState extends State<AddAppoinment> {
                 const SizedBox(
                   height: 10,
                 ),
-                
               ],
             ),
           ),
