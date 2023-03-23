e6745922d56bea06b2a82bae4fe20faa43a93762
RushiniThiranya <IT19129440@my.sliit.lk>
2023-03-23 21:23:11 +0530

    main:Drugs Details List UI Design

--- lib/drug_home/edit_drugs_info.dart
+++ lib/drug_home/edit_drugs_info.dart
@@ -1,130 +1,142 @@
-import 'package:factory_management_ctse/data/models/apponment_model.dart';
-import 'package:factory_management_ctse/data/remote_data_source/appoinment_helper.dart';
-import 'package:flutter/material.dart';
+// import 'package:factory_management_ctse/data/models/drugs_model.dart';
+// import 'package:factory_management_ctse/data/remote_data_source/drug_helper.dart';
+// import 'package:flutter/material.dart';
 
-class EditAppoinmentnfo extends StatefulWidget {
-  final AppoinmentModel appoinment;
-  const EditAppoinmentnfo({Key? key, required this.appoinment}) : super(key: key);
+// class EditDruginfo extends StatefulWidget {
+//   final DrugsModel drug;
+//   const EditDruginfo({Key? key, required this.drug}) : super(key: key);
 
-  @override
-  State<EditAppoinmentnfo> createState() => _EditAppoinmentnfoState();
-}
+//   @override
+//   State<EditDruginfo> createState() => _EditDruginfoState();
+// }
 
-class _EditAppoinmentnfoState extends State<EditAppoinmentnfo> {
-  TextEditingController? _doctorNamecontroller;
-  TextEditingController? _hospitalNamecontroller;
-  TextEditingController? _datecontroller;
-  TextEditingController? _resoncontroller;
-  final _formKey = GlobalKey<FormState>();
+// class _EditDruginfoState extends State<EditDruginfo> {
+//   TextEditingController? _drCodecontroller;
+//   TextEditingController? _drNamecontroller;
+//   TextEditingController? _unitPriceontroller;
+//   TextEditingController? _drCategorycontroller;
+//   TextEditingController? _drStatuscontroller;
+//   final _formKey = GlobalKey<FormState>();
 
-  @override
-  void initState() {
-    _doctorNamecontroller = TextEditingController(text: widget.appoinment.doctorName);
-    _hospitalNamecontroller = TextEditingController(text: widget.appoinment.hospitalName);
-    _datecontroller = TextEditingController(text: widget.appoinment.date);
-    _resoncontroller = TextEditingController(text: widget.appoinment.reson);
-    //init state
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     _drCodecontroller = TextEditingController(text: widget.drug.drCode);
+//     _drNamecontroller = TextEditingController(text: widget.drug.drName);
+//     _unitPriceontroller = TextEditingController(text: widget.drug.unitPrice);
+//     _drCategorycontroller = TextEditingController(text: widget.drug.drCategory);
+//     _drStatuscontroller = TextEditingController(text: widget.drug.drStatus);
+//     //init state
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-    _doctorNamecontroller!.dispose();
-    _hospitalNamecontroller!.dispose();
-    _datecontroller!.dispose();
-    _resoncontroller!.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _drCodecontroller!.dispose();
+//     _drNamecontroller!.dispose();
+//     _unitPriceontroller!.dispose();
+//     _drCategorycontroller!.dispose();
+//     _drStatuscontroller!.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Edit Appoinment Info"),
-          backgroundColor: const Color.fromARGB(255, 17, 90, 150),
-          elevation: 0.0,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _hospitalNamecontroller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Hospital"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: _doctorNamecontroller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Doctor Name"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: _datecontroller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Date"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: _resoncontroller,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), hintText: "Reson for appoinment"),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                InkWell(
-                  onTap: () {
-                    AppoinmentHelper.update(AppoinmentModel(
-                            id: widget.appoinment.id,
-                            hospitalName: _hospitalNamecontroller!.text,
-                            doctorName: _doctorNamecontroller!.text, 
-                            date: _datecontroller!.text, 
-                            reson: _resoncontroller!.text, 
-                            ))
-                        .then((value) {
-                      Navigator.pop(context);
-                    });
-                  },
-                  child: Container(
-                    width: 100,
-                    height: 30,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.green),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.update,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "Modify",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Edit Drug Info"),
+//           backgroundColor: const Color.fromARGB(255, 17, 90, 150),
+//           elevation: 0.0,
+//         ),
+//         body: Padding(
+//           padding: const EdgeInsets.all(8),
+//           child: Form(
+//             key: _formKey,
+//             child: Column(
+//               children: [
+//                 TextFormField(
+//                   controller: _drNamecontroller,
+//                   decoration: const InputDecoration(
+//                       border: OutlineInputBorder(), hintText: "Drug Name"),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 TextFormField(
+//                   controller: _drCodecontroller,
+//                   decoration: const InputDecoration(
+//                       border: OutlineInputBorder(), hintText: "Drug Code"),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 TextFormField(
+//                   controller: _unitPriceontroller,
+//                   decoration: const InputDecoration(
+//                       border: OutlineInputBorder(), hintText: "Unit Price"),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 TextFormField(
+//                   controller: _drCategorycontroller,
+//                   decoration: const InputDecoration(
+//                       border: OutlineInputBorder(), hintText: "Drug Category"),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 TextFormField(
+//                   controller: _drStatuscontroller,
+//                   decoration: const InputDecoration(
+//                       border: OutlineInputBorder(), hintText: "Drug Status"),
+//                 ),
+//                 const SizedBox(
+//                   height: 10,
+//                 ),
+//                 InkWell(
+//                   onTap: () {
+//                     DrugHelper.update(DrugsModel(
+//                             id: widget.drug.id,
+//                             drCode: _drCodecontroller!.text,
+//                             drName: _drNamecontroller!.text,
+//                             unitPrice: _unitPriceontroller!.text,
+//                             drCategory: _drCategorycontroller!.text,
+//                             drStatus: _drStatuscontroller!.text
+//                             ))
+//                         .then((value) {
+//                       Navigator.pop(context);
+//                     });
+//                   },
+//                   child: Container(
+//                     width: 100,
+//                     height: 30,
+//                     decoration: BoxDecoration(
+//                         borderRadius: BorderRadius.circular(20),
+//                         color: Colors.green),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: const [
+//                         Icon(
+//                           Icons.update,
+//                           color: Colors.white,
+//                         ),
+//                         SizedBox(
+//                           width: 5,
+//                         ),
+//                         Text(
+//                           "Modify",
+//                           style: TextStyle(color: Colors.white),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
