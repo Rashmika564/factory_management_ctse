e77c8f0169bdf7cdf0ac4490906bf075eaa4b257
RushiniThiranya <IT19129440@my.sliit.lk>
2023-03-23 12:38:47 +0530

    main:drugs model changes

--- lib/data/models/drugs.dart
+++ lib/data/models/drugs.dart
@@ -8,18 +8,31 @@ class DrugsModel {
   final String? drCategory;
   final String? drStatus;
 
-  DrugsModel({this.drCode, this.drName,this.unitPrice,this.drCategory , this.drStatus , this.id});
+  DrugsModel(
+      {this.drCode,
+      this.drName,
+      this.unitPrice,
+      this.drCategory,
+      this.drStatus,
+      this.id});
   factory DrugsModel.fromSnapshot(DocumentSnapshot snap) {
     var snapshot = snap.data() as Map<String, dynamic>;
     return DrugsModel(
       drCode: snapshot['drCode'],
       drName: snapshot['drName'],
       unitPrice: snapshot['unitPrice'],
-      drCategory: snapshot['drCategory'], 
+      drCategory: snapshot['drCategory'],
       drStatus: snapshot['drStatus'],
       id: snapshot['id'],
     );
   }
 
-  Map<String, dynamic> toJson() => {"drCode": drCode, "drName": drName, "unitPrice": unitPrice, "drCategory":drCategory, "drStatus":drStatus,  "id": id};
+  Map<String, dynamic> toJson() => {
+        "drCode": drCode,
+        "drName": drName,
+        "unitPrice": unitPrice,
+        "drCategory": drCategory,
+        "drStatus": drStatus,
+        "id": id
+      };
 }
